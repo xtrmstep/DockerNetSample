70f8f0ed7d9dcb8873328257993bbdbcc1325c91
Alex <alexander.g@btigroup.io>
2020-01-17 09:00:35 +0200

    Added grpc client app

---
+++ src/LoaderClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using WorkerService;
+
+namespace LoaderClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Worker.WorkerClient(channel);
+            var reply = await client.LoadAsync(new LoadRequest {Seconds = 10});
+
+            Console.WriteLine("Loaded: " + reply.NumberOfMessages);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
